441c49a1862da52fab6c1de9e155a4ba72409ac2
Amit <amitkale.281995@gmail.com>
2024-07-05 11:36:28 +0530

    addition program done

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Write a progarm to sum of 1 to n
+#solution
+
+n=int(input("Enter no upto which you want to sum:  "))
+i=1
+sum=0
+while(i<=n):
+    sum=sum+i
+    i=i+1
+print("sum= ",sum
+      )
